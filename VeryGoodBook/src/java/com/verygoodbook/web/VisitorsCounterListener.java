f520b2ce5287c62bcadfc018f2980b1314ad6a98
YingRuiHuang <bu820115@gmail.com>
2017-02-24 14:57:44 +0800

    0224造訪人數進階版

--- VeryGoodBook/src/java/com/verygoodbook/web/VisitorsCounterListener.java
+++ VeryGoodBook/src/java/com/verygoodbook/web/VisitorsCounterListener.java
@@ -5,6 +5,14 @@
  */
 package com.verygoodbook.web;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -19,15 +27,58 @@ import javax.servlet.http.HttpSessionListener;
  */
 //@WebListener
 public class VisitorsCounterListener implements ServletContextListener, HttpSessionListener {
-
+    private String filePath="/WEB-INF/vgb.properties";
     @Override
     public void contextInitialized(ServletContextEvent sce) {
        //TODO:
+        System.out.println("contest Initialized");
+        ServletContext application=sce.getServletContext();        
+        String path=application.getRealPath(filePath);
+        System.out.println("path="+path);
+        Properties props=new Properties();
+        try (FileReader reader=new FileReader(path)){
+           props.load(reader);
+        } catch (IOException ex) {
+            Logger.getLogger(VisitorsCounterListener.class.getName()).log(Level.SEVERE,
+                    "讀取系統設定檔["+path+"]失敗", ex);
+        }
+        
+       Enumeration names= props.propertyNames();
+       while(names.hasMoreElements()){
+           String name =(String)names.nextElement();
+           String value=props.getProperty(name);
+           if(value!=null&&value.matches("\\d+")){
+               application.setAttribute(name, Integer.parseInt(value));
+           }else{
+                application.setAttribute(name,value);
+           }
+       }
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
        //TODO:
+        System.out.println("context Destoryed");
+        ServletContext application=sce.getServletContext();        
+        Properties props=new Properties();
+        
+       Enumeration<String> names=application.getAttributeNames();
+       while(names.hasMoreElements()){
+           String name=names.nextElement();
+           if(name.indexOf("app.")>-1){
+              Object value=application.getAttribute(name);
+              props.setProperty(name, value.toString());
+           }
+          
+       }
+      //FileWriter 
+       String path=application.getRealPath(filePath);
+        try (FileWriter writer=new FileWriter(filePath)){
+            props.store(writer,"VGB context Destoryed:store Application attributes!");
+        } catch (IOException ex) {
+            Logger.getLogger(VisitorsCounterListener.class.getName()).log(Level.SEVERE,
+                    "寫入系統設定檔失敗", ex);
+        }
     }
 
     @Override
